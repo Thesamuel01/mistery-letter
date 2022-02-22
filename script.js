661ea25443683f817d04729a3e5389167f1d0afa
samuel santos <samaraujo28@gmail.com>
2022-02-21 22:08:33 -0300

    Quinto requisito atendido

--- script.js
+++ script.js
@@ -13,17 +13,32 @@ function resetBoard() {
   board.innerHTML = '';
 }
 
+function validateInput(input) {
+  let isValidInput = true;
+
+  if (input.length === 0) {
+    board.innerHTML = 'Por favor, digite o conteúdo da carta.';
+    isValidInput = false;
+  }
+
+  return isValidInput;
+}
+
 function generateLetters() {
   const inputText = document.querySelector('#carta-texto').value;
-  const texts = inputText.split(' ');
+  // Trecho baseado no link: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+  const texts = inputText.split(' ').filter((value) => value !== '');
+  const isInputEmpty = validateInput(texts);
 
-  resetBoard();
+  if (isInputEmpty) {
+    resetBoard();
 
-  for (let index = 0; index < texts.length; index += 1) {
-    const word = texts[index];
-    const span = createLetter(word);
+    for (let index = 0; index < texts.length; index += 1) {
+      const word = texts[index];
+      const span = createLetter(word);
 
-    board.appendChild(span);
+      board.appendChild(span);
+    }
   }
 }
 
