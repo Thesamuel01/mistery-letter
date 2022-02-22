0353e1cf782c7fd35ca09e6f1a3364bece3c5da9
samuel santos <samaraujo28@gmail.com>
2022-02-21 22:34:21 -0300

    Decimo sexto requisito atendido

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 const button = document.querySelector('#criar-carta');
 const board = document.querySelector('#carta-gerada');
 
+function getRandomIndex(array) {
+  const randomIndex = Math.floor(Math.random() * array.length);
+
+  return randomIndex;
+}
+
+function addRandomClass(span) {
+  const style = ['newspaper', 'magazine1', 'magazine2'];
+  const size = ['medium', 'big', 'reallybig'];
+  const rotation = ['rotateleft', 'rotateright'];
+  const inclination = ['skewleft', 'skewright'];
+  const styleSheet = [style, size, rotation, inclination];
+
+  for (let index = 0; index < styleSheet.length; index += 1) {
+    const classes = styleSheet[index];
+    const className = classes[getRandomIndex(classes)];
+
+    span.classList.add(className);
+  }
+}
+
 function createLetter(text) {
   const span = document.createElement('span');
 
   span.textContent = text;
+  addRandomClass(span);
 
   return span;
 }
